40894f90824344f573577a732a86be27180bc1b8
Kirill Puzanov <kiryjm@gmail.com>
2023-11-28 02:00:37 +0400

    Add SolidPrinciples app, single responsibility

---
+++ solid-principles-app/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingleResponsibility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+var j = new Journal();
+j.AddEntry("I cried today");
+j.AddEntry("I write code");
+Console.WriteLine();
+
+ var p = new PersistenceManager();
+      var filename = @"D:\My programs\journal.txt";
+      p.SaveToFile(j, filename);
+      
+      var psi = new ProcessStartInfo();
+      psi.FileName = filename;
+      psi.UseShellExecute = true;
+      Process.Start(psi);
---
+++ solid-principles-app/Single responsibility/SingleResponsibility.cs
@@ -0,0 +1,57 @@
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility {
+
+   // just stores a couple of journal entries and ways of
+  // working with them
+  public class Journal
+  {
+    private readonly List<string> entries = new List<string>();
+
+    private static int count = 0;
+
+    public void AddEntry(string text)
+    {
+      entries.Add(text);
+    }
+
+    public void RemoveEntry(int index)
+    {
+      if (index < entries.Count && index >= 0)
+        entries.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+      return string.Join(Environment.NewLine, entries);
+    }
+
+    // breaks single responsibility principle
+    public void Save(string filename, bool overwrite = false)
+    {
+      File.WriteAllText(filename, ToString());
+    }
+
+    public void Load(string filename)
+    {
+      
+    }
+
+    public void Load(Uri uri)
+    {
+      
+    }
+  }
+
+  // handles the responsibility of persisting objects
+  public class PersistenceManager
+  {
+    public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+    {
+      if (overwrite || !File.Exists(filename))
+        File.WriteAllText(filename, journal.ToString());
+    }
+  }
+}
